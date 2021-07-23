46db9d646115299c19a56b503a9ddae005cb7074
Lee GyeongJun <cockyb@nate.com>
2021-07-23 17:14:16 +0900

    Feat: Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Logo() {
+  return (
+    <div>
+      <span className='text-3xl'>발</span>음&nbsp;&nbsp;
+      <span className='text-3xl'>정</span>해주는&nbsp;&nbsp;
+      <span className='bg-orange-400 text-3xl text-black rounded-md p-1'>
+        남<span className='text-base'>자들&nbsp;&nbsp;</span>
+      </span>
+    </div>
+  );
+}
