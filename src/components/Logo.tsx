624c52207eb7900e5e1d1b4044627a6c58f36bb6
Lee GyeongJun <cockyb@nate.com>
2021-07-25 16:18:21 +0900

    Update: Logo component ui

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useTheme} from 'next-themes';
 
 export default function Logo() {
+  const [mounted, setMounted] = useState(false);
+  const {theme} = useTheme();
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
   return (
-    <div>
-      <span className='text-3xl'>발</span>음&nbsp;&nbsp;
-      <span className='text-3xl'>정</span>해주는&nbsp;&nbsp;
-      <span className='bg-orange-400 text-3xl text-black rounded-md p-1'>
-        남<span className='text-base'>자들&nbsp;&nbsp;</span>
-      </span>
-    </div>
+    <>
+      {mounted && theme === 'dark' ? 
+        <div className='transition-all'>
+          <span className='text-3xl'>발</span>음&nbsp;&nbsp;
+          <span className='text-3xl'>정</span>해주는&nbsp;&nbsp;
+          <span className='bg-orange-400 text-3xl text-black rounded-md p-1'>
+            남<span className='text-base'>자들&nbsp;&nbsp;</span>
+          </span>
+        </div>:
+        <div>Pronunciation Convention</div>
+      }
+    </>
   );
 }
