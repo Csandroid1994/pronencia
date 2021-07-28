75da2e7e3c27d9faa31f0fe10de3d84eed515a28
Lee GyeongJun <cockyb@nate.com>
2021-07-28 16:43:33 +0900

    Update: backgroud effect & navigation bar opacity

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({children}: LayoutProps) {
   return (
-    <div className='w-full flex justify-center dark:bg-gray-700'>
-      <div className='w-1152 px-3 h-screen flex flex-col justify-center items-center'>
-        {children}
+    <>
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+      <div className='w-full flex justify-center'>
+        <div className='w-1152 px-3 h-screen'>
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Logo from './Logo';
 import ThemeSwitch from './ThemeSwitch';
+import {BiUserCircle} from 'react-icons/bi';
 
 export default function Navigation() {
   return (
-    <nav className='flex sticky top-0 bg-white w-full border-b-2 justify-center shadow-md dark:bg-gray-600 dark:border-gray-500'>
+    <nav className='flex sticky bg-white opacity-90 w-full border-b-2 py-3 justify-center shadow-md dark:bg-black dark:border-gray-500'>
       <div className='h-16 flex space-y-0 justify-between items-center w-1152 px-3'>
         <Logo />
-        <ul className='flex gap-x-4 antialiased'>
+        <ul className='flex gap-x-4 antialiased text-3xl'>
           <ThemeSwitch />
+          <BiUserCircle />
         </ul>
       </div>
     </nav>
