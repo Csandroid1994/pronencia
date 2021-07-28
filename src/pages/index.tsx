f4690c5a7635a2778766ced9e86344092c77cd66
Lee GyeongJun <cockyb@nate.com>
2021-07-28 16:46:40 +0900

    Update: index page
    adjust layout & searchbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import Navigation from 'components/Navigation';
+import React, {useEffect} from 'react';
+
 import Layout from 'components/Layout';
 import SearchBar from 'components/SearchBar';
 
 export default function Home() {
   return (
     <>
-      <Navigation />
       <Layout>
-        <SearchBar />
+        <div className='w-full h-full flex justify-center items-center'>
+          <SearchBar />
+        </div>
       </Layout>
     </>
   );
