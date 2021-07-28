e39c448162e1e8984758a8bd512b86faa95e2c41
Lee GyeongJun <cockyb@nate.com>
2021-07-28 16:43:47 +0900

    Update: Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useTheme} from 'next-themes';
+import Link from 'next/link';
 
 export default function Logo() {
   const [mounted, setMounted] = useState(false);
@@ -9,17 +10,17 @@ export default function Logo() {
   useEffect(() => setMounted(true), []);
 
   return (
-    <>
+    <Link href='/'>
       {mounted && theme === 'dark' ? 
-        <div className='transition-all'>
+        <a>
           <span className='text-3xl'>발</span>음&nbsp;&nbsp;
           <span className='text-3xl'>정</span>해주는&nbsp;&nbsp;
           <span className='bg-orange-400 text-3xl text-black rounded-md p-1'>
             남<span className='text-base'>자들&nbsp;&nbsp;</span>
           </span>
-        </div>:
-        <div>Pronunciation Convention</div>
+        </a>:
+        <a className='text-2xl'>Pronunciation Convention</a>
       }
-    </>
+    </Link>
   );
 }
