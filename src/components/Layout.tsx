0a1d14c950c0e28db91deecafbd4aa5fb77b32c4
Lee GyeongJun <cockyb@nate.com>
2021-07-29 17:44:20 +0900

    Update: backgroud effect

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,9 +6,6 @@ interface LayoutProps {
 export default function Layout({children}: LayoutProps) {
   return (
     <>
-      <div id='stars'></div>
-      <div id='stars2'></div>
-      <div id='stars3'></div>
       <div className='w-full flex justify-center'>
         <div className='w-1152 px-3 h-screen'>
           {children}
