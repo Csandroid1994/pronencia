a15270c56e59d40c473ba40bb55f9c73c40852c5
Lee GyeongJun <cockyb@nate.com>
2021-08-04 17:35:41 +0900

    Update: search page UI

--- src/pages/search/[word].tsx
+++ src/pages/search/[word].tsx
@@ -6,14 +6,34 @@ import Layout from 'components/Layout';
 import SearchBar from 'components/SearchBar';
 import PostCard from 'components/PostCard';
 
+interface PostCardProps {
+  id: string;
+  word: string;
+  mostLikePronunciation: string;
+  helpfulCount: number;
+  tags: string[];
+  opinionCount: number;
+  viewCount: number;
+  createdAt: string;
+}
+
 const posts = [{
   id: '1',
   word: 'ASUS',
-  pronunciation: '아수스',
-  likes: 15,
-  hates: 22,
-  opinions: 1,
-  views: 131,
+  mostLikePronunciation: '아수스',
+  helpfulCount: 15,
+  tags: ['프론트', '백엔드'],
+  opinionCount: 1,
+  viewCount: 131,
+  createdAt: new Date().toLocaleDateString(),
+}, {
+  id: '2',
+  word: 'Ajax',
+  mostLikePronunciation: '에이작스',
+  helpfulCount: 15,
+  tags: ['프론트', '백엔드'],
+  opinionCount: 1,
+  viewCount: 131,
   createdAt: new Date().toLocaleDateString(),
 }];
 
@@ -24,17 +44,19 @@ export default function Search() {
   return (
     <Layout>
       <Head>
-        <title>{word}의 발음은? - 발.정.남</title>
+        <title>{word}에 대한 검색 결과 - 발.정.남</title>
       </Head>
       <div className='flex flex-col items-start'>
         <div className='flex justify-between w-full items-baseline'>
-          <h1 className='text-white text-5xl mt-24 mb-10'>&apos;{word}&apos;에 대한 검색 결과</h1>
+          <h1 className='dark:text-white text-5xl mt-24 mb-10'>&apos;{word}&apos;에 대한 검색 결과</h1>
           <SearchBar />
         </div>
-        <hr className='border-b-2 border-blueGray-300 w-full' />
+        <hr className='border-b-2 border-gray-300 w-full' />
       </div>
-      {posts.map((value) => {
-        return <PostCard key={value.id} data={value}/>;
+      {posts.map((value: PostCardProps) => {
+        return <div key={value.id}>
+          <PostCard data={value}/>
+        </div>;
       })}
     </Layout>
   );
