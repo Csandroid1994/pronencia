72ef52f842aac9eaaa76d78994118efd881f7932
Lee GyeongJun <cockyb@nate.com>
2021-08-05 15:20:38 +0900

    Update: navigation ui

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,8 @@ import {BiUserCircle} from 'react-icons/bi';
 
 export default function Navigation() {
   return (
-    <nav className='flex sticky bg-white opacity-90 w-full border-b-2 py-3 justify-center shadow-md dark:bg-black dark:border-gray-500'>
+    <nav className='flex sticky top-0 bg-white opacity-90 w-full border-b-2 py-3 justify-center shadow-md 
+    dark:bg-black dark:border-gray-500'>
       <div className='h-16 flex space-y-0 justify-between items-center w-1152 px-3'>
         <Logo />
         <ul className='flex gap-x-4 antialiased text-3xl'>
